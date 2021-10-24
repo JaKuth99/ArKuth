ef300a1a7e42cbb5a4671f376624d0d348505092
JaKoby Cuthbertson <jrcuthbertson99@gmail.com>
2021-10-23 23:46:55 -0400

    forgotten fork typo

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -106,7 +106,7 @@ linux /vmlinuz-linux
 initrd  /initramfs-linux.img  
 options root=LABEL=ROOT rw rootflags=subvol=@
 EOF
-cp -R ~/ArchTitus /mnt/root/
+cp -R ~/ArKuth /mnt/root/
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 echo "--------------------------------------"
 echo "--   SYSTEM READY FOR 0-setup       --"
