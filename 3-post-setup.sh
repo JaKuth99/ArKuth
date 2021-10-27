b4099a01557c974ce47192186b4c250e2f18c7bf
JaKoby Cuthbertson <jrcuthbertson99@gmail.com>
2021-10-27 15:36:39 -0400

    Added light dm

--- 3-post-setup.sh
+++ 3-post-setup.sh
@@ -22,6 +22,7 @@ sudo systemctl enable ntpd.service
 sudo systemctl disable dhcpcd.service
 sudo systemctl stop dhcpcd.service
 sudo systemctl enable NetworkManager.service
+sudo systemctl enable lightdm.service
 echo "
 ###############################################################################
 # Cleaning
